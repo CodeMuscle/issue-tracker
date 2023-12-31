eff26d5c37dbf6141b41d532d742c374c9f67717
CodeMuscle <investportal99@gmail.com>
2023-12-31 10:39:12 +0530

    Add a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar'
-import { Theme } from '@radix-ui/themes'
+import { Theme, ThemePanel } from '@radix-ui/themes'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 const metadata: Metadata = {
   title: 'Issue Tracker',
-  description: 'Find and fix all the issues on your behalf',
+  description: 'Find and fix all the issues on your project',
 }
 
 export default function RootLayout({
@@ -19,10 +23,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
           <main className="p-5">{children}</main>
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
